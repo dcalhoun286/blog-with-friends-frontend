d691ef11ffb5e36060cf334590584d0951e3e042
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2022-12-09 10:23:51 -0800

    initial commit error page

---
+++ src/ErrorPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <React.Fragment>
+            Uh oh! An error occurred.
+        </React.Fragment>
+    );
+};
+
+export default ErrorPage;
