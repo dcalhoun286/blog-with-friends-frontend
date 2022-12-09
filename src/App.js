be6aebc3949b8df188a64b40be4562c3ea179296
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2022-12-09 10:50:03 -0800

    moved fetching and rendering users logic into Users component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 const App = () => {
-
-  const [users, setUsers] = useState([]);
-
-  const fetchUsers = async () => {
-
-    const res = await fetch('http://127.0.0.1:8000/users');
-    const jsonData = await res.json();
-    const arrayOfUsers = jsonData.data;
-    setUsers(arrayOfUsers);
-  };
-
-
-  useEffect(() => {
-
-    fetchUsers();
-    // eslint-disable-next-line
-  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Blog With Friends</h1>
-        {
-          !users.isEmpty
-            ? (
-            
-            <ul>
-              {users.map(userRecord => (
-                <li key={userRecord.id}>
-                  <h2>{`${userRecord.firstname} ${userRecord.lastname}`}</h2>
-                  <p>{`username: ${userRecord.username}`}</p>
-                </li>
-              ))}
-            </ul>
-          ) : null
-        }
       </header>
     </div>
   );
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,6 +1,40 @@
+import React, { useState, useEffect } from 'react';
+
 const Users = () => {
+
+    const [users, setUsers] = useState([]);
+
+    const fetchUsers = async () => {
+
+        const res = await fetch('http://127.0.0.1:8000/users');
+        const jsonData = await res.json();
+        const arrayOfUsers = jsonData.data;
+        setUsers(arrayOfUsers);
+    };
+
+    useEffect(() => {
+
+        fetchUsers();
+        // eslint-disable-next-line
+    }, []);
+
     return (
-        <div>temp text to prove my route is working</div>
+        <React.Fragment>
+            {
+                !users.isEmpty
+                    ? (
+                    
+                    <ul>
+                    {users.map(userRecord => (
+                        <li key={userRecord.id}>
+                        <h2>{`${userRecord.firstname} ${userRecord.lastname}`}</h2>
+                        <p>{`username: ${userRecord.username}`}</p>
+                        </li>
+                    ))}
+                    </ul>
+                ) : null
+            }
+        </React.Fragment>
     )
 }
 
