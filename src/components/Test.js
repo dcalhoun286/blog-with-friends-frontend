6201bed06e750130d11e2d860898b1b1d8cfa263
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2022-12-09 11:00:17 -0800

    initial commit Test component

---
+++ src/components/Test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Test = () => {
+    return (
+        <React.Fragment>
+            <div>Test component proof of life</div>
+        </React.Fragment>
+    );
+};
+
+export default Test;
