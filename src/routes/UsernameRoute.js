88070fd65b3cd07c1623d1c6ace501ef2ab3cbd5
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2022-12-12 22:10:37 -0800

    added params functionality to UsernameRoute

--- src/routes/UsernameRoute.js
+++ src/routes/UsernameRoute.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 import Username from '../components/Username';
 
 const UsernameRoute = () => {
+
+    const params = useParams();
+
     return (
-        {}
+        <Username username={params.username}/>
     );
 };
 
