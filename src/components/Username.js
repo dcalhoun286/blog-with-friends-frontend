295c69d09d25061d6a3da635e47ad350b13ce49b
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2022-12-12 18:49:35 -0800

    initial commit Username

---
+++ src/components/Username.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Username = () => {
+    return (
+        {}
+    );
+};
+
+export default Username;
---
+++ src/routes/UsernameRoute.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Username from '../components/Username';
+
+const UsernameRoute = () => {
+    return (
+        {}
+    );
+};
+
+export default UsernameRoute;
